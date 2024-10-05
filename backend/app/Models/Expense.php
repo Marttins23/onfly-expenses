5750657d841902da08fa4a193065810bf88ab94d
Marttins23 <mateus_martins18@yahoo.com.br>
2024-10-05 16:16:03 -0300

    feat: Adicionar Model e Migration da tabela 'expenses'.

---
+++ backend/app/Models/Expense.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'description',
+        'value',
+        'user_id',
+    ];
+
+    /**
+     * Get the user that owns the expense.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
