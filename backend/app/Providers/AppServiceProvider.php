b3047134e8a6304cad6af712c30c5a2cde29651b
Marttins23 <mateus_martins18@yahoo.com.br>
2024-10-09 06:29:33 -0300

    chore: Registrar Policies e implementacoes de interfaces no container de servicos do Laravel.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,15 +3,34 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use App\Services\User\UserServiceInterface;
+use App\Services\User\EloquentUserApiService;
+use App\Repositories\User\EloquentUserRepository;
+use App\Services\Expense\ExpenseServiceInterface;
+use App\Services\Expense\EloquentExpenseApiService;
+use App\Repositories\Expense\EloquentExpenseRepository;
+use App\Models\Expense;
+use App\Policies\ExpensePolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        Expense::class => ExpensePolicy::class
+    ];
+
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+        App::bind(UserServiceInterface::class, function () {
+            return new EloquentUserApiService(new EloquentUserRepository());
+        });
+
+        App::bind(ExpenseServiceInterface::class, function () {
+            return new EloquentExpenseApiService(new EloquentExpenseRepository());
+        });
     }
 
     /**
